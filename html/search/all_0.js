28e7bff20cc24086ad12eafdb29bcccb72bc8cd6
_______ <94187567+JahyLuky@users.noreply.github.com>
2023-06-23 23:11:53 +0200

    Delete html directory

--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['chess_20game_20_28zadání_20_22hra_3a_20Šachy_22_29_0',['Chess game (zadání &quot;Hra: Šachy&quot;)',['../md_README.html',1,'']]]
-];
--- html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['my_20project_20documentation_1',['My Project Documentation',['../md_DOCUMENTATION.html',1,'']]]
-];
